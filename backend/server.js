163a5d75e2bc9c971b40ee6eb831a8efd793cbae
Rudra0 <prudra882@gmail.com>
2020-06-16 22:09:54 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -78,7 +78,7 @@ app.use("/api/products", productRoute);
 app.use(express.static(path.join(__dirname, 'koob/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../koob/build/index.html`));
-  console.log(res.sendFile)
+  console.log(res.sendFile.path)
 });
 
 app.listen(PORT,()=>{
