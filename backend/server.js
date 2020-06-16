6dba323ce36ab3e9ceddccea2b74db946219eda2
Rudra0 <prudra882@gmail.com>
2020-06-17 00:03:54 +0530

    dine

--- backend/server.js
+++ backend/server.js
@@ -76,9 +76,11 @@ app.use("/api/products", productRoute);
 // }
 
 app.use(express.static(path.join(__dirname, '/../koob/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(`${__dirname}/../koob/build/index.html`));
+app.get('/*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../koob', 'build', 'index.html'));
 });
+
+
 
 app.listen(PORT,()=>{
     console.log("connected");
